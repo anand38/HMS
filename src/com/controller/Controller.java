9573cc2d7724224a0d343d17526ad630b3f4a882
anand38 <anandbrahmadev@gmail.com>
2017-05-24 19:19:36 +0530

    Third commit

--- src/com/controller/Controller.java
+++ src/com/controller/Controller.java
@@ -1,6 +1,5 @@
 package com.controller;
 
-import com.model.Account;
 import com.model.JobID;
 import com.model.JobOperation;
 
@@ -10,10 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.DateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Random;
 
 public class Controller extends HttpServlet {
 
